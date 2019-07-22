b5ef66f6071f2eaa8a16fffc2e648780ff95de3d
LGValdez <lg.valdez1995@gmail.com>
2019-07-22 03:57:24 -0400

    user story 4 updated

--- katas/03-bank_ocr/gabriel.py
+++ katas/03-bank_ocr/gabriel.py
@@ -42,20 +42,6 @@ translator = {
     number_9: "9"
 }
 
-often_mistakes = {
-    "0": ["8"],
-    "1": ["7"],
-    "2": ["3"],
-    "3": ["9"],
-    "4": ["4"],
-    "5": ["6", "9"],
-    "6": ["5", "8"],
-    "7": ["1"],
-    "8": ["0", "6", "9"],
-    "9": ["5", "8", "3"],
-    "?": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-}
-
 
 def validate_account_number(account_number):
     if "?" in account_number:
@@ -64,6 +50,19 @@ def validate_account_number(account_number):
     for index, char in enumerate(reversed(list(account_number))):
         checksum += (index+1) * int(char)
     return checksum % 11 == 0
+
+
+def get_numbers(file):
+    numbers = {0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: ''}
+    for line in file:
+        index = 0
+        for i in range(0, 27, 3):
+            if (not line[i:i + 3] and line != "\n") or line == "\n":
+                numbers[index] += "   "
+            else:
+                numbers[index] += line[i:i + 3].replace("\n", " ")
+            index += 1
+    return numbers
 
 
 def translate_numbers(numbers):
@@ -78,15 +77,7 @@ def translate_numbers(numbers):
 
 def read_file(file_path):
     f = open(file_path)
-    numbers = {0: '', 1: '', 2: '', 3: '', 4: '', 5: '', 6: '', 7: '', 8: ''}
-    for line in f:
-        index = 0
-        for i in range(0, 27, 3):
-            if (not line[i:i+3] and line != "\n") or line == "\n":
-                numbers[index] += "   "
-            else:
-                numbers[index] += line[i:i + 3].replace("\n", " ")
-            index += 1
+    numbers = get_numbers(f)
     f.close()
     return translate_numbers(numbers)
 
@@ -100,19 +91,44 @@ def get_status(file_path):
     return account_number
 
 
-def get_possibles(file_path):
-    account_number = read_file(file_path)
+def do_rotation(number):
+    list_possible = []
+    for index, char in enumerate(number):
+        possible = list(number)
+        for value in [" ", "_", "|"]:
+            possible[index] = value
+            if "".join(possible) in translator:
+                list_possible.append(translator["".join(possible)])
+    return list(dict.fromkeys(list_possible))
+
+
+def get_values(numbers, index):
+    account_number = translate_numbers(numbers)
     possible_values = []
-    if "?" in account_number or not validate_account_number(account_number):
-        for index, char in enumerate(account_number):
-            for rotation in often_mistakes[char]:
-                possible = list(account_number)
-                possible[index] = rotation
-                if validate_account_number("".join(possible)):
-                    possible_values.append("".join(possible))
+    for option in do_rotation(numbers[index]):
+        possible = list(account_number)
+        possible[index] = option
+        if validate_account_number("".join(possible)):
+            possible_values.append("".join(possible))
+    return possible_values
+
+
+def get_possibles(file_path):
+    f = open(file_path)
+    numbers = get_numbers(f)
+    f.close()
+    account_number = translate_numbers(numbers)
+    possible_values = []
+    if "?" in account_number:
+        for index in [i for i, e in enumerate(list(account_number)) if e == "?"]:
+            possible_values += get_values(numbers, index)
+    elif not validate_account_number(account_number):
+        for index in range(len(account_number)):
+            possible_values += get_values(numbers, index)
     if not possible_values:
         return account_number + " ILL"
     elif len(possible_values) == 1:
         return possible_values[0]
     else:
         return account_number + " AMB " + str(sorted(possible_values))
+
